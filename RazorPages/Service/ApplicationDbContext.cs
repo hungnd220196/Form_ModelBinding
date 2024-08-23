89cebaf3702c8bc451533160d89f5b81057540df
nguyenduchung <nguyenduchung22011996@gmail.com>
2024-08-23 17:17:28 +0700

    Add project files.

---
+++ RazorPages/Service/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPages.Service
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext( DbContextOptions options):base(options) 
+        
+        {
+            
+        }
+    }
+}
